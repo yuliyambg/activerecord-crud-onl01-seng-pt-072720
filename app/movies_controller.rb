2785fa5ba2dcbd1a0f736cd0657a344d99add55c
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:37:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,7 +84,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.create(title: "Movie_#{i}", release_date: 2000+i)
 end
 
 def can_destroy_a_single_item
