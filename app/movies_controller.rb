4638a3025c58ee40f78bbc58da7566fe763189e4
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:34:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -75,8 +75,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  id=Movie.where(title: "Wat?").first
+  Movie.update
 end
 
 def can_update_multiple_items_at_once
