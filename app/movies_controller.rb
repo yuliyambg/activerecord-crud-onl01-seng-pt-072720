82cb44ea94ec8a81784a77e7479b3a8937b5fa26
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:27:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -55,7 +55,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where("title=='Title'", "release_date == 2000","director == 'Me'")
+  Movie.where("title=='Title'", "release_date == 2000","director == 'Me'").first
 end
 
 def can_find_using_where_clause_and_be_sorted
