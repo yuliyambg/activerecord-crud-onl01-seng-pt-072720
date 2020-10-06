ca525afb03fc89ce25fecc7e1c980fc7a69a4a04
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:09:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args =nil)
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = args[:title] || "Home Alone"
-    m.release_date = args[:release_date] || 1990
+    m.title = args[:title] unless args || "Home Alone"
+    m.release_date = args[:release_date] unless args || 1990
   end
 end
 
