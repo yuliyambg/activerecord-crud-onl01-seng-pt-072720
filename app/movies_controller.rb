f1b3c66ec72404a922a0a7b90faf8c19731f249b
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:31:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,8 +68,8 @@ def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
   id=Movie.where(title: "Awesome Flick").first.id
-  Movie.update(title: )
-  __
+  Movie.update(title: "Even Awesomer Flick")
+  Movie.find(id)
 end
 
 def can_update_using_update_method
