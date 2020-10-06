c054777058dd27ef4f9fc8728698986bb917e207
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-06 05:39:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -89,7 +89,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   __
 end
 
